9cd8eaad25f4267cf916530250a2c4f1a0961830
Kostas4S <81780796+Kostas4S@users.noreply.github.com>
2022-01-05 18:07:00 -0500

    Add files via upload

---
+++ Screens/factsScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Text, View} from 'react-native'
+
+export default class FactsScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Facts about animals
+                </Text>
+            </View>
+        )
+    }
+}
